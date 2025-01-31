73acaafcd9a8b8801da8a7700410e98ed34bdb6a
Leah Xia <107075081+Leah-Xia-Microsoft@users.noreply.github.com>
2025-01-30 18:04:21 -0800

    Add Privacy and Terms of use pages

--- AgentApp/src/components/Privacy.tsx
+++ AgentApp/src/components/Privacy.tsx
@@ -1,6 +1,9 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+import { Text } from '@fluentui/react-components';
+import { useLegalComplianceStyles } from '../styles/LegalCompliance.styles';
+
 /**
  * This component is used to display the required
  * privacy statement which can be found in a link in the
@@ -8,9 +11,40 @@
  */
 
 export const Privacy = (): JSX.Element => {
+  const styles = useLegalComplianceStyles();
   return (
-    <div>
-      <h1>Privacy Statement</h1>
+    <div className={styles.container}>
+      <Text className={styles.title}>Privacy Statement</Text>
+      <div className={styles.sectionContainer}>
+        <Text className={styles.description}>
+          <strong>Please add your privacy statement before going to production.</strong>
+        </Text>
+      </div>
+      <div className={styles.sectionContainer}>
+        {' '}
+        <Text className={styles.description}>
+          Privacy statement is mandatory for all apps submitted to the Teams Store.
+        </Text>
+        <Text className={styles.description}>
+          For more information, please refer to the Privacy policy section of the{' '}
+          <a href="https://learn.microsoft.com/en-us/microsoftteams/platform/concepts/deploy-and-publish/appsource/prepare/teams-store-validation-guidelines#privacy-policy">
+            Teams Store validation guidelines.
+          </a>
+        </Text>
+      </div>
+
+      <div className={styles.sectionContainer}>
+        <Text className={styles.description}>
+          If you have a privacy policy page hosted elsewhere, instead of using this component, you can directly link to
+          the external page by modify the privacyUrl property of the manifest.json file.
+        </Text>
+        <Text className={styles.description}>
+          For more information, please refer to{' '}
+          <a href="https://learn.microsoft.com/en-us/microsoftteams/platform/resources/schema/manifest-schema#developer">
+            App manifest.
+          </a>
+        </Text>
+      </div>
     </div>
   );
 };
--- AgentApp/src/components/TermsOfUse.tsx
+++ AgentApp/src/components/TermsOfUse.tsx
@@ -1,15 +1,47 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+import { Text } from '@fluentui/react-components';
+import { useLegalComplianceStyles } from '../styles/LegalCompliance.styles';
+
 /**
  * This component is used to display the required
  * terms of use statement which can be found in a
  * link in the about tab.
  */
 export const TermsOfUse = (): JSX.Element => {
+  const styles = useLegalComplianceStyles();
   return (
-    <div>
-      <h1>Terms of Use</h1>
+    <div className={styles.container}>
+      <Text className={styles.title}>Terms of Use</Text>
+      <div className={styles.sectionContainer}>
+        <Text className={styles.description}>
+          <strong>Please add your terms of use before going to production.</strong>
+        </Text>
+      </div>
+      <div className={styles.sectionContainer}>
+        {' '}
+        <Text className={styles.description}>Terms of use is mandatory for all apps submitted to the Teams Store.</Text>
+        <Text className={styles.description}>
+          For more information, please refer to the terms of use section of the{' '}
+          <a href="https://learn.microsoft.com/en-us/microsoftteams/platform/concepts/deploy-and-publish/appsource/prepare/teams-store-validation-guidelines#terms-of-use">
+            Teams Store validation guidelines.
+          </a>
+        </Text>
+      </div>
+
+      <div className={styles.sectionContainer}>
+        <Text className={styles.description}>
+          If you have a terms of use page hosted elsewhere, instead of using this component, you can directly link to
+          the external page by modify the termsOfUseUrl property of the manifest.json file.
+        </Text>
+        <Text className={styles.description}>
+          For more information, please refer to{' '}
+          <a href="https://learn.microsoft.com/en-us/microsoftteams/platform/resources/schema/manifest-schema#developer">
+            App manifest.
+          </a>
+        </Text>
+      </div>
     </div>
   );
 };
