1d70edc031ad829681b89a453d0f6048fe1c11f4
Leah Xia <107075081+Leah-Xia-Microsoft@users.noreply.github.com>
2025-03-28 15:34:02 -0700

    Remove unnecessary style comments

--- CustomerApp/src/app/styles/ErrorScreen.styles.ts
+++ CustomerApp/src/app/styles/ErrorScreen.styles.ts
@@ -15,11 +15,11 @@ export const useErrorScreenStyles = makeStyles({
   },
   closeButtonContainer: {
     alignSelf: 'flex-end',
-    margin: '0.375rem' // 6px
+    margin: '0.375rem'
   },
   closeButton: {
-    width: '2rem', // 32px
-    height: '2rem', // 32px
+    width: '2rem',
+    height: '2rem',
     padding: 0,
     color: tokens.colorNeutralForeground2,
     border: 'none'
@@ -29,21 +29,21 @@ export const useErrorScreenStyles = makeStyles({
     flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center',
-    gap: '0.25rem', // 4px
+    gap: '0.25rem',
     width: '100%',
     height: '319px'
   },
   errorIcon: {
     color: tokens.colorNeutralStroke1Pressed,
-    marginBottom: '0.375rem' // 6px
+    marginBottom: '0.375rem'
   },
   errorTitle: {
-    fontSize: '1.125rem', // 18px
+    fontSize: '1.125rem',
     fontWeight: 600,
-    lineHeight: '1.375rem' // 22px
+    lineHeight: '1.375rem'
   },
   errorMessage: {
-    fontSize: '0.875rem', // 14px
+    fontSize: '0.875rem',
     fontWeight: 400,
     lineHeight: '18.62px'
   },
@@ -51,11 +51,11 @@ export const useErrorScreenStyles = makeStyles({
     display: 'flex',
     flexDirection: 'column',
     width: '100%',
-    gap: '0.5rem', // 8px
-    marginBottom: '0.75rem' // 12px
+    gap: '0.5rem',
+    marginBottom: '0.75rem'
   },
   button: {
     height: '32px',
-    margin: '0 0.75rem' // 0 12px
+    margin: '0 0.75rem'
   }
 });
--- CustomerApp/src/app/styles/GetLiveHelpButton.styles.ts
+++ CustomerApp/src/app/styles/GetLiveHelpButton.styles.ts
@@ -6,23 +6,23 @@ import { makeStyles, tokens } from '@fluentui/react-components';
 export const useGetLiveHelpButtonStyles = makeStyles({
   button: {
     fontWeight: 600,
-    fontSize: '0.875rem', // 14px
-    lineHeight: '1.375rem', // 22px
-    letterSpacing: '-0.06em', // 0.01px
-    width: '8.813rem', // 141px
-    height: '2.375rem', // 38px
+    fontSize: '0.875rem',
+    lineHeight: '1.375rem',
+    letterSpacing: '-0.06em',
+    width: '8.813rem',
+    height: '2.375rem',
     boxShadow: '0px 2px 21px 0px #003C6A33',
     border: 'none',
-    borderRadius: '1.125rem', // 18px
+    borderRadius: '1.125rem',
     position: 'fixed',
-    right: '0.5625rem', // 9px
-    bottom: '0.75rem', // 12px
+    right: '0.5625rem',
+    bottom: '0.75rem',
     color: tokens.colorCompoundBrandForeground1Pressed,
     ':hover': {
       color: tokens.colorCompoundBrandForeground1Hover
     }
   },
   chatIcon: {
-    fontSize: '1.25rem' // 20px
+    fontSize: '1.25rem'
   }
 });
